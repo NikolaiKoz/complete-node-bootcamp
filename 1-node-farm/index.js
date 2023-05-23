64df54708ba35e56930648982e1d5fdf2d4e8f1f
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-05-23 19:45:48 -0300

    feat: :art: Write and create a new file with Node.

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-const hello = 'Hello world';
-console.log(hello);
+console.log(textIn);
+
+const textOut = `This is what we know about the advocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt', textOut);
+
+console.log('File written!');
