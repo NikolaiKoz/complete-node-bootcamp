dfb44c99d79a7f7dd29a23a6175c4e64726c0c15
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-05-24 11:37:57 -0300

    fix: :art: Callback hell!

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,11 +1,31 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+//Codigo bloqueandte y sincronico
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
 
-console.log(textIn);
+// const textOut = `This is what we know about the advocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
 
-const textOut = `This is what we know about the advocado: ${textIn}.\nCreated on ${Date.now()}`;
+//Codigo NO bloqueante y Asincronico.
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR 🦖");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(`data2: ${data2}`);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(`data3: ${data3}`);
 
-fs.writeFileSync('./txt/output.txt', textOut);
+      fs.writeFile(
+        "./txt/final.txt",
+        `${data2}\n\n\n${data3}`,
+        "utf-8",
+        (err) => {
+          console.log("Your file has been written 😄");
+        }
+      );
+    });
+  });
+});
 
-console.log('File written!');
+console.log("Will read file");
