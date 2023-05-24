de9b8138f14f6c7b9a3dca92b4c763c32e523ad9
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-05-24 17:48:46 -0300

    perf: :bookmark: FIrst proyect WORKS!

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -68,12 +68,11 @@ const tempProduct = fs.readFileSync(
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   /////////////////////////
   // Overview page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -85,12 +84,15 @@ const server = http.createServer((req, res) => {
 
     /////////////////////////
     // Products page
-  } else if (pathName === "/product") {
-    res.end("This is the Products");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     /////////////////////////
     // API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
 
