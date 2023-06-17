2dcc50a72034e72b25fe71aee4da7c1be0c85aa5
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-06-17 12:26:32 -0300

    feat: :sparkles: V52 WIP.

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'This is a get method' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can post to this endpoint');
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('api/v1/tours/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+        tours
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}...`);
+});
